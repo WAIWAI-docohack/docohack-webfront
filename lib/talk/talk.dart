86d0bf55718c88e7fd7a7537b1fc938f71992517
Yuki-arl <yukimk1224@gmail.com>
2021-09-15 01:48:27 +0900

    errorなう

--- lib/talk/talk.dart
+++ lib/talk/talk.dart
@@ -6,6 +6,18 @@ import 'package:ring_sns/page/home.dart';
 import 'package:flutter/rendering.dart';
 import 'package:ring_sns/page/like-dislike.dart';
 
+import 'dart:async';
+
+import 'package:agora_rtc_engine/rtc_engine.dart';
+import 'package:agora_rtc_engine/rtc_local_view.dart' as RtcLocalView;
+import 'package:agora_rtc_engine/rtc_remote_view.dart' as RtcRemoteView;
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+const appId = "aa46abd9d4f04d06a57b2804ec0135c9";
+const token = "006aa46abd9d4f04d06a57b2804ec0135c9IADSZS1TaOFyyez5vzkNFPS5dIrONFHPsjNrNc0+qN0jPgx+f9gAAAAAIgD+bihbWRtCYQQAAQA/G0JhAgA/G0JhAwA/G0JhBAA/G0Jh";
+
+
 class Talk extends StatefulWidget {
   Talk(this.auth, this.room_id, this.target_userid);
   Auth auth;
@@ -79,5 +91,97 @@ class _Talk extends State<Talk> {
         ),
       ),
     );
+  }
+}
+
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int? _remoteUid;
+  bool _localUserJoined = false;
+  late RtcEngine _engine;
+
+  @override
+  void initState() {
+    super.initState();
+    initAgora();
+  }
+
+  Future<void> initAgora() async {
+    // retrieve permissions
+    await [Permission.microphone, Permission.camera].request();
+
+    //create the engine
+    _engine = await RtcEngine.create(appId);
+    await _engine.enableVideo();
+    _engine.setEventHandler(
+      RtcEngineEventHandler(
+        joinChannelSuccess: (String channel, int uid, int elapsed) {
+          print("local user $uid joined");
+          setState(() {
+            _localUserJoined = true;
+          });
+        },
+        userJoined: (int uid, int elapsed) {
+          print("remote user $uid joined");
+          setState(() {
+            _remoteUid = uid;
+          });
+        },
+        userOffline: (int uid, UserOfflineReason reason) {
+          print("remote user $uid left channel");
+          setState(() {
+            _remoteUid = null;
+          });
+        },
+      ),
+    );
+
+    await _engine.joinChannel(token, "test", null, 0);
+  }
+
+  // Create UI with local view and remote view
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Agora Video Call'),
+      ),
+      body: Stack(
+        children: [
+          Center(
+            child: _remoteVideo(),
+          ),
+          Align(
+            alignment: Alignment.topLeft,
+            child: Container(
+              width: 100,
+              height: 150,
+              child: Center(
+                child: _localUserJoined
+                    ? RtcLocalView.SurfaceView()
+                    : CircularProgressIndicator(),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // Display remote user's video
+  Widget _remoteVideo() {
+    if (_remoteUid != null) {
+      return RtcRemoteView.SurfaceView(uid: _remoteUid!);
+    } else {
+      return Text(
+        'Please wait for remote user to join',
+        textAlign: TextAlign.center,
+      );
+    }
   }
 }
